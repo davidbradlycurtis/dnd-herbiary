10a2dfb1e8ec4c10df5b1d8b84c413c6826e829f
davidbradlycurtis <s531274@nwmissouri.edu>
2021-01-01 22:29:54 -0600

    Added clickable model

--- custom-elements/compendium-table.js
+++ custom-elements/compendium-table.js
@@ -5,30 +5,43 @@
 // copyright (c) 2020
 // all rights reserved.
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-function drawTable() {
+function drawTable(plants) {
     // get the reference for the body
-    var table = document.getElementById('coldiv');
+    var table = document.getElementById('table');
 
     // get reference for <table> element
-    var tbl = document.getElementById("plant-table");
+    var tbody = document.getElementById("tbody");
 
     // creating rows
-    for (var r = 0; r < plants_data.length; r++) {
+    for (var r = 0; r < plants.length; r++) {
         var row = document.createElement("tr");
 
      // create cells in row
-         for (let property in plants_data[r]) {
+         for (let attribute in plants[r]) {
             var cell = document.createElement("td");
-            var cellText = document.createElement('span');
-            cellText.innerText = plants_data[r[property]];
-            cell.appendChild(cellText);
+            var plant = plants[r];
+            if (attribute === "biome" || attribute === "property") {
+                cell.innerText = plant[attribute].join(", ");
+            }
+            else {
+                cell.innerText = plant[attribute];
+            }
             row.appendChild(cell);
         }           
 
-tbl.appendChild(row); // add the row to the end of the table body
+        tbody.appendChild(row); // add the row to the end of the table body
     }
 
- table.appendChild(tbl); // appends <table> into <div>
+    table.appendChild(tbody); // appends <table> into <div>
+    $('#table').bootstrapTable({
+        onClickRow: function (row, $element) {
+
+            $('#plantModal').modal();
+
+            var message = "Row: " + row.name;
+            //alert(message);
+        }
+      })
 }
 
-drawTable();
+drawTable(plants);
--- custom-elements/plant-data/plant-tags.js
+++ custom-elements/plant-data/plant-tags.js
@@ -12,19 +12,34 @@ Each plant will have the following attributes:
 name (String): the name of the plant
 biome (arr[String]): the biomes the plant can appear [Rainforest, Temperate Forest, Desert, Tundra, Grassland, Savanna, Underdark, Swamp, Any]
 type (String): the type of plant [Tree, Fungus, Flower, Herb, Moss, Shrub, Weed, Grass]
-rarity (String): the rarity of the plant [Common, Uncommon, Rare, Very Rare, Legendary]
+rarity (String): the rarity of the plant [Common, Uncommon, Rare, Legendary]
 property (arr[String]): shorthand special effect of the plant [Hazardous, Benificial, Material, Neutral, None]
+
+    {
+        name: "",
+        biome: [""],
+        type: "",
+        rarity: "",
+        property: [""]
+    },
 
 */
 
-var plant_data = [
+var plants = [
+    {
+        name: "Aadarna",
+        biome: ["Swamp", "Forest"],
+        type: "Flower",
+        rarity: "Very Rare",
+        property: ["Material"]
+    },
 
     {
         name: "Abyssal Blackgrass",
         biome: ["Underdark"],
         type: "Weed",
         rarity: "Uncommon",
-        property: ["Hazardous"],
+        property: ["Hazardous"]
     }
 
 ]
